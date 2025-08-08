8a30c80560c844668e40774c9493ba4252bfded1
Andy Ron <randy.njfu@gmail.com>
2025-08-08 12:05:16 +0800

    修正bug，测试文件地址。验证

--- route.go
+++ route.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"github.com/orgming/mingdemo/framework"
-	"github.com/orgming/mingdemo/framework/middleware"
 )
 
 // registerRouter 注册路由规则
 func registerRouter(core *framework.Core) {
 	// 需求1，2：HTTP方法，静态路由匹配
 	// 在core中使用middleware.Test3() 为单个路由增加中间件
-	core.Get("/user/login", middleware.Test3(), UserLoginController)
+	core.Get("/user/login", UserLoginController)
 
 	// 需求3：批量通用前缀
 	subjectApi := core.Group("/subject")
-	subjectApi.Use(middleware.Test3())
 	{
 		// 需求4：动态路由
 		// 在group中使用middleware.Test3() 为单个路由增加中间件
-		subjectApi.Get("/:id", middleware.Test3(), SubjectGetController)
+		subjectApi.Get("/:id", SubjectGetController)
 		subjectApi.Put("/:id", SubjectUpdateController)
 		subjectApi.Delete("/:id", SubjectDeleteController)
 		subjectApi.Get("/list/all", SubjectListController)
