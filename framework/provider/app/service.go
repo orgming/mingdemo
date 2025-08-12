d69bedec2452e4efc949306fcafebec1e9456c3d
Andy Ron <randy.njfu@gmail.com>
2025-08-12 20:16:52 +0800

    配置文件更新 App 服务

--- framework/provider/app/service.go
+++ framework/provider/app/service.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"errors"
-	"flag"
 	"github.com/google/uuid"
 	"github.com/orgming/ming/framework"
 	"github.com/orgming/ming/framework/util"
@@ -18,6 +17,10 @@ type MingApp struct {
 	configMap map[string]string // 配置加载
 }
 
+func (app MingApp) AppID() string {
+	return app.appID
+}
+
 func (app MingApp) Version() string {
 	return "0.0.1"
 }
@@ -28,40 +31,83 @@ func (app MingApp) BaseFolder() string {
 		return app.baseFolder
 	}
 	// 没有设置就是用命令行参数
-	var baseFolder string
-	flag.StringVar(&baseFolder, "base_folder", "", "base_folder参数，默认为当前路径")
-	flag.Parse()
-	if baseFolder != "" {
-		return baseFolder
-	}
+	//var baseFolder string
+	//flag.StringVar(&baseFolder, "base_folder", "", "base_folder参数，默认为当前路径")
+	//flag.Parse()
+	//if baseFolder != "" {
+	//	return baseFolder
+	//}
 
 	return util.GetExecDirectory()
 }
 
 func (app MingApp) ConfigFolder() string {
+	if val, ok := app.configMap["config_folder"]; ok {
+		return val
+	}
 	return filepath.Join(app.BaseFolder(), "config")
 }
 
 func (app MingApp) LogFolder() string {
-	return filepath.Join(app.BaseFolder(), "log")
+	if val, ok := app.configMap["log_folder"]; ok {
+		return val
+	}
+	return filepath.Join(app.StorageFolder(), "log")
 }
+
+func (app MingApp) HttpFolder() string {
+	if val, ok := app.configMap["http_folder"]; ok {
+		return val
+	}
+	return filepath.Join(app.BaseFolder(), "http")
+}
+
+func (app MingApp) ConsoleFolder() string {
+	if val, ok := app.configMap["console_folder"]; ok {
+		return val
+	}
+	return filepath.Join(app.BaseFolder(), "console")
+}
+
+func (app MingApp) StorageFolder() string {
+	if val, ok := app.configMap["storage_folder"]; ok {
+		return val
+	}
+	return filepath.Join(app.BaseFolder(), "storage")
+}
+
 func (app MingApp) ProviderFolder() string {
+	if val, ok := app.configMap["provider_folder"]; ok {
+		return val
+	}
 	return filepath.Join(app.BaseFolder(), "provider")
 }
 
 func (app MingApp) MiddlewareFolder() string {
-	return filepath.Join(app.BaseFolder(), "middleware")
+	if val, ok := app.configMap["middleware_folder"]; ok {
+		return val
+	}
+	return filepath.Join(app.HttpFolder(), "middleware")
 }
 
 func (app MingApp) CommandFolder() string {
-	return filepath.Join(app.BaseFolder(), "command")
+	if val, ok := app.configMap["command_folder"]; ok {
+		return val
+	}
+	return filepath.Join(app.ConsoleFolder(), "command")
 }
 
 func (app MingApp) RuntimeFolder() string {
-	return filepath.Join(app.BaseFolder(), "runtime")
+	if val, ok := app.configMap["runtime_folder"]; ok {
+		return val
+	}
+	return filepath.Join(app.StorageFolder(), "runtime")
 }
 
 func (app MingApp) TestFolder() string {
+	if val, ok := app.configMap["test_folder"]; ok {
+		return val
+	}
 	return filepath.Join(app.BaseFolder(), "test")
 }
 
