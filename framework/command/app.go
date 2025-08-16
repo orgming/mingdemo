06c6ea96948d8658b2db16c71e6aff21d28683a5
Andy Ron <randy.njfu@gmail.com>
2025-08-16 18:15:04 +0800

    完善Web服务运行命令

--- framework/command/app.go
+++ framework/command/app.go
@@ -2,23 +2,35 @@ package command
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/erikdubbelboer/gspt"
+	"github.com/orgming/ming/framework"
 	"github.com/orgming/ming/framework/cobra"
 	"github.com/orgming/ming/framework/contract"
-	"log"
+	"github.com/orgming/ming/framework/util"
+	"github.com/sevlyar/go-daemon"
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"strconv"
 	"syscall"
 	"time"
 )
 
 // app启动地址 类似localhost:8888 或者 :8888
 var appAddress = ""
+var appDaemon = false
 
 func initAppCmd() *cobra.Command {
+	appStartCmd.Flags().BoolVarP(&appDaemon, "daemon", "d", false, "start app daemon")
 	appStartCmd.Flags().StringVar(&appAddress, "address", ":8888", "设置app启动的地址，默认为：8888")
 
 	appCmd.AddCommand(appStartCmd)
+	appCmd.AddCommand(appRestartCmd)
+	appCmd.AddCommand(appStateCmd)
+	appCmd.AddCommand(appStopCmd)
 	return appCmd
 }
 
@@ -33,6 +45,35 @@ var appCmd = &cobra.Command{
 	},
 }
 
+// 启动AppServer, 这个函数会将当前goroutine阻塞
+func startAppServe(server *http.Server, c framework.Container) error {
+	// 这个goroutine是启动服务的goroutine
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	// 当前的goroutine等待信号量
+	quit := make(chan os.Signal)
+	// 监控信号：SIGINT, SIGTERM, SIGQUIT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// 这里会阻塞当前goroutine等待信号
+	<-quit
+
+	// 调用Server.Shutdown graceful结束
+	closeWait := 5
+	configService := c.MustMake(contract.ConfigKey).(contract.Config)
+	if configService.IsExist("app.close_wait") {
+		closeWait = configService.GetInt("app.close_wait")
+	}
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), time.Duration(closeWait)*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(timeoutCtx); err != nil {
+		return err
+	}
+	return nil
+}
+
 // 启动一个web服务
 var appStartCmd = &cobra.Command{
 	Use:   "start",
@@ -44,28 +85,218 @@ var appStartCmd = &cobra.Command{
 		// 从kernel服务实例中获取引擎
 		engine := kernelService.HttpEngine()
 
+		if appAddress == "" {
+			envService := container.MustMake(contract.EnvKey).(contract.Env)
+			if envService.Get("ADDRESS") != "" {
+				appAddress = envService.Get("ADDRESS")
+			} else {
+				configService := container.MustMake(contract.ConfigKey).(contract.Config)
+				if configService.IsExist("app.address") {
+					appAddress = configService.GetString("app.address")
+				} else {
+					appAddress = ":8888"
+				}
+			}
+		}
+
 		server := &http.Server{
 			Addr:    appAddress,
 			Handler: engine,
 		}
 
-		go func() {
-			server.ListenAndServe()
-		}()
+		appService := container.MustMake(contract.AppKey).(contract.App)
 
-		// 当前的goroutine等待信号量
-		quit := make(chan os.Signal)
-		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-		// 阻塞当前goroutine等待信号
-		<-quit
-
-		timeoutCtx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancelFunc()
-
-		if err := server.Shutdown(timeoutCtx); err != nil {
-			log.Fatal("Server Shutdown: ", err)
+		pidFolder := appService.RuntimeFolder()
+		if !util.Exists(pidFolder) {
+			if err := os.MkdirAll(pidFolder, os.ModePerm); err != nil {
+				return err
+			}
+		}
+		serverPidFile := filepath.Join(pidFolder, "app.pid")
+		logFolder := appService.LogFolder()
+		if !util.Exists(logFolder) {
+			if err := os.MkdirAll(logFolder, os.ModePerm); err != nil {
+				return err
+			}
+		}
+		// 应用日志
+		serverLogFile := filepath.Join(logFolder, "app.log")
+		currentFolder := util.GetExecDirectory()
+		// daemon 模式
+		if appDaemon {
+			// 创建一个Context
+			cxt := &daemon.Context{
+				// 设置pid文件
+				PidFileName: serverPidFile,
+				PidFilePerm: 0664,
+				// 设置日志文件
+				LogFileName: serverLogFile,
+				LogFilePerm: 0640,
+				// 设置工作路径
+				WorkDir: currentFolder,
+				// 设置所有设置文件的mask，默认为750
+				Umask: 027,
+				// 子进程的参数，按照这个参数设置，子进程的命令为 ./ming app start --daemon=true
+				Args: []string{"", "app", "start", "--daemon=true"},
+			}
+			// 启动子进程，d不为空表示当前是父进程，d为空表示当前是子进程
+			d, err := cxt.Reborn()
+			if err != nil {
+				return err
+			}
+			if d != nil {
+				// 父进程直接打印启动成功信息，不做任何操作
+				fmt.Println("app启动成功，pid:", d.Pid)
+				fmt.Println("日志文件:", serverLogFile)
+				return nil
+			}
+			defer cxt.Release()
+			// 子进程执行真正的app启动操作
+			fmt.Println("daemon started")
+			gspt.SetProcTitle("ming app")
+			if err := startAppServe(server, container); err != nil {
+				fmt.Println(err)
+			}
+			return nil
 		}
 
+		// 非daemon模式，直接执行
+		content := strconv.Itoa(os.Getpid())
+		fmt.Println("[PID]", content)
+		err := os.WriteFile(serverPidFile, []byte(content), 0644)
+		if err != nil {
+			return err
+		}
+		gspt.SetProcTitle("ming app")
+
+		fmt.Println("app serve url:", appAddress)
+		if err := startAppServe(server, container); err != nil {
+			fmt.Println(err)
+		}
+
+		return nil
+	},
+}
+
+var appRestartCmd = &cobra.Command{
+	Use:   "restart",
+	Short: "重新启动一个app服务",
+	RunE: func(c *cobra.Command, args []string) error {
+		container := c.GetContainer()
+		appService := container.MustMake(contract.AppKey).(contract.App)
+
+		// GetPid
+		serverPidFile := filepath.Join(appService.RuntimeFolder(), "app.pid")
+
+		content, err := os.ReadFile(serverPidFile)
+		if err != nil {
+			return err
+		}
+
+		if content != nil && len(content) != 0 {
+			pid, err := strconv.Atoi(string(content))
+			if err != nil {
+				return err
+			}
+			if util.CheckProcessExist(pid) {
+				// 杀死进程
+				if err := syscall.Kill(pid, syscall.SIGTERM); err != nil {
+					return err
+				}
+
+				// 获取closeWait
+				closeWait := 5
+				configService := container.MustMake(contract.ConfigKey).(contract.Config)
+				if configService.IsExist("app.close_wait") {
+					closeWait = configService.GetInt("app.close_wait")
+				}
+
+				// 确认进程已经关闭,每秒检测一次， 最多检测closeWait * 2秒
+				for i := 0; i < closeWait*2; i++ {
+					if util.CheckProcessExist(pid) == false {
+						break
+					}
+					time.Sleep(1 * time.Second)
+				}
+
+				// 如果进程等待了2*closeWait之后还没结束，返回错误，不进行后续的操作
+				if util.CheckProcessExist(pid) == true {
+					fmt.Println("结束进程失败:"+strconv.Itoa(pid), "请查看原因")
+					return errors.New("结束进程失败")
+				}
+				if err := os.WriteFile(serverPidFile, []byte{}, 0644); err != nil {
+					return err
+				}
+
+				fmt.Println("结束进程成功:" + strconv.Itoa(pid))
+			}
+		}
+
+		appDaemon = true
+		// 直接daemon方式启动apps
+		return appStartCmd.RunE(c, args)
+	},
+}
+
+var appStopCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "停止一个已经启动的app服务",
+	RunE: func(c *cobra.Command, args []string) error {
+		container := c.GetContainer()
+		appService := container.MustMake(contract.AppKey).(contract.App)
+
+		// GetPid
+		serverPidFile := filepath.Join(appService.RuntimeFolder(), "app.pid")
+
+		content, err := os.ReadFile(serverPidFile)
+		if err != nil {
+			return err
+		}
+
+		if content != nil && len(content) != 0 {
+			pid, err := strconv.Atoi(string(content))
+			if err != nil {
+				return err
+			}
+			// 发送SIGTERM命令
+			if err := syscall.Kill(pid, syscall.SIGTERM); err != nil {
+				return err
+			}
+			if err := os.WriteFile(serverPidFile, []byte{}, 0644); err != nil {
+				return err
+			}
+			fmt.Println("停止进程:", pid)
+		}
+		return nil
+	},
+}
+
+var appStateCmd = &cobra.Command{
+	Use:   "state",
+	Short: "获取启动的app的pid",
+	RunE: func(c *cobra.Command, args []string) error {
+		container := c.GetContainer()
+		appService := container.MustMake(contract.AppKey).(contract.App)
+
+		// 获取pid
+		serverPidFile := filepath.Join(appService.RuntimeFolder(), "app.pid")
+
+		content, err := os.ReadFile(serverPidFile)
+		if err != nil {
+			return err
+		}
+
+		if content != nil && len(content) > 0 {
+			pid, err := strconv.Atoi(string(content))
+			if err != nil {
+				return err
+			}
+			if util.CheckProcessExist(pid) {
+				fmt.Println("app服务已经启动, pid:", pid)
+				return nil
+			}
+		}
+		fmt.Println("没有app服务存在")
 		return nil
 	},
 }
