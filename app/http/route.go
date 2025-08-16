499ac8db14ceb34610428088af810e946352f81a
Andy Ron <randy.njfu@gmail.com>
2025-08-16 10:18:53 +0800

    修改路由

--- app/http/route.go
+++ app/http/route.go
@@ -3,9 +3,14 @@ package http
 import (
 	"github.com/orgming/ming/app/http/module/demo"
 	"github.com/orgming/ming/framework/gin"
+	"github.com/orgming/ming/framework/middleware/static"
 )
 
 func Routes(r *gin.Engine) {
-	r.Static("/dist/", "./dist/")
+	//r.Static("/dist/", "./dist/")
+
+	// /路径先去./dist目录下查找文件是否存在，找到使用文件服务提供服务
+	r.Use(static.Serve("/", static.LocalFile("./dist", false)))
+
 	demo.Register(r)
 }
